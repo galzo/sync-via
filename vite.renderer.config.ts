367ef3ac1462c0d481d370223822c26f3ef2811c
Gal Zohar <galz@ai21.com>
2024-08-03 21:10:02 +0300

    added typescript type checks to vite build

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import { pluginExposeRenderer } from './vite.base.config';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config
 export default defineConfig((env) => {
@@ -23,6 +24,7 @@ export default defineConfig((env) => {
       eslint({
         include: ['src/**/*.ts', 'src/**/*.tsx'],
       }),
+      checker({ typescript: true }),
     ],
     resolve: {
       preserveSymlinks: true,
